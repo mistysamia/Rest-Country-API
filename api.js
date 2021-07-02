f02d75f692c8feb9c4abf37b1883f98f8cfc55fa
Misty Samia <misty.samia17@gmail.com>
2021-07-03 04:22:01 +0600

    3rd Commit

--- api.js
+++ api.js
@@ -1,41 +1,47 @@
 fetch('https://restcountries.eu/rest/v2/all')
     .then((response) => response.json())
     .then((json) => {
+        console.log(json);
         CountrySet(json);
     });
 
 const CountrySet = (CountryArray) => {
     CountryArray.forEach(element => {
-  
         const CountryDiv = document.getElementById('Country-Div');
         const countryDivDetails = document.createElement('div');
-        const AddImage = document.createElement('div');
-        const AllDetails = document.createElement('div');
-
+       // const countryDetails = document.createElement('div');
+        let countryDetails = "";
+    
         const url = `https://restcountries.eu/rest/v2/name/${element.name}`;
         fetch(url)
             .then((response) => response.json())
             .then((data) => {
-         
-                const countryDetails = `
+                countryDetails += `
+                <div id="${element.alpha2Code}-Details" 
+                onmouseover="DetailsShow('${element.name}','${element.alpha2Code}')" onmouseout="DetailsOff('${element.name}','${element.alpha2Code}')">
+                    <img src="${data[0].flag}" class="Flag-img" >
                     <h2>${element.name}</h2>
                     <p>${element.capital}</p>
-                  `
-                const imageDetails=`
-                     <img src="${data[0].flag}" class="Flag-img">
+                </div>
                 `
-                AddImage.className = "Image-Details";
-                AddImage.innerHTML = imageDetails;
-                AllDetails.appendChild(AddImage);
-
-                countryDivDetails.className = "Country-Details";
+                countryDivDetails.className = 'Country-Details';
                 countryDivDetails.innerHTML = countryDetails;
-                AllDetails.appendChild(countryDivDetails);
-                
-                AllDetails.className = "All-Details";
-                CountryDiv.appendChild(AllDetails);
-
+                CountryDiv.appendChild(countryDivDetails);
+    
             });
-    }); 
+    });
 }
 
+
+
+function DetailsShow(name,code){
+    const CountryDiv = document.getElementById(`${code}-Details`);    
+    CountryDiv.style.display="none";
+
+}
+function DetailsOff(name,code){
+    const CountryDiv = document.getElementById(`${code}-Details`);
+   
+    CountryDiv.style.display="block";
+
+}
