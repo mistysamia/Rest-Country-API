630339eb899d481f10b99661e5d6801fb1d28a30
Misty Samia <misty.samia17@gmail.com>
2021-07-03 05:59:38 +0600

    Bye Bye Pore abar update korbo

--- api.js
+++ api.js
@@ -7,9 +7,10 @@ fetch('https://restcountries.eu/rest/v2/all')
 
 const CountrySet = (CountryArray) => {
     CountryArray.forEach(element => {
+    
         const CountryDiv = document.getElementById('Country-Div');
         const countryDivDetails = document.createElement('div');
-       // const countryDetails = document.createElement('div');
+        const countryHoverDetails = document.createElement('div');
         let countryDetails = "";
     
         const url = `https://restcountries.eu/rest/v2/name/${element.name}`;
@@ -17,17 +18,24 @@ const CountrySet = (CountryArray) => {
             .then((response) => response.json())
             .then((data) => {
                 countryDetails += `
-                <div id="${element.alpha2Code}-Details" 
+                <div id="${element.alpha2Code}" 
                 onmouseover="DetailsShow('${element.name}','${element.alpha2Code}')" onmouseout="DetailsOff('${element.name}','${element.alpha2Code}')">
                     <img src="${data[0].flag}" class="Flag-img" >
                     <h2>${element.name}</h2>
-                    <p>${element.capital}</p>
                 </div>
+                <div id="${element.alpha2Code}-Details"  class="Country-Hover-Details"
+                onmouseover="DetailsShow('${element.name}','${element.alpha2Code}')" onmouseout="DetailsOff('${element.name}','${element.alpha2Code}')">
+                    <h2>Capital:  ${element.capital}</h2>
+                    &nbsp;<h4>Region: ${data[0].region}</h4>
+                    <h4>Area:  ${data[0].area}</h4>
+                    <h4>Population:${data[0].population}</h4>
+                </div>
+
                 `
                 countryDivDetails.className = 'Country-Details';
                 countryDivDetails.innerHTML = countryDetails;
                 CountryDiv.appendChild(countryDivDetails);
-    
+
             });
     });
 }
@@ -35,13 +43,14 @@ const CountrySet = (CountryArray) => {
 
 
 function DetailsShow(name,code){
-    const CountryDiv = document.getElementById(`${code}-Details`);    
+    const CountryDiv = document.getElementById(`${code}`);    
+    const CountryHoverDiv = document.getElementById(`${code}-Details`); 
     CountryDiv.style.display="none";
-
+    CountryHoverDiv.style.display="block";
 }
 function DetailsOff(name,code){
-    const CountryDiv = document.getElementById(`${code}-Details`);
-   
+    const CountryDiv = document.getElementById(`${code}`);
+    const CountryHoverDiv = document.getElementById(`${code}-Details`);
     CountryDiv.style.display="block";
-
+    CountryHoverDiv.style.display="none";
 }
