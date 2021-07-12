af7a6f512f22c7779eea05a921fe0aed9cb50112
Misty Samia <misty.samia17@gmail.com>
2021-07-12 06:11:06 +0600

    Updated

--- api.js
+++ api.js
@@ -21,11 +21,11 @@ const CountrySet = (CountryArray) => {
                 <div id="${element.alpha2Code}" 
                 onmouseover="DetailsShow('${element.name}','${element.alpha2Code}')" onmouseout="DetailsOff('${element.name}','${element.alpha2Code}')">
                     <img src="${data[0].flag}" class="Flag-img" >
-                    <h3 class="text-md-center">${element.name}</h3>
                 </div>
                 <div id="${element.alpha2Code}-Details"  class="Country-Hover-Details"
                 onmouseover="DetailsShow('${element.name}','${element.alpha2Code}')" onmouseout="DetailsOff('${element.name}','${element.alpha2Code}')">
-                    <h2 class="text-md-center">Capital:  ${element.capital}</h2>
+                    <h2 class="text-md-center City">Country:  ${element.name}</h2>
+                    <h2 class="text-md-center Capital">Capital:  ${element.capital}</h2>
                     &nbsp;<h4>Region: ${data[0].region}</h4>
                     <h4 class="text-md-center">Area:  ${data[0].area}<span> &#13218;</span></h4>
                     <h4 class="text-md-center">Population:${data[0].population}</h4>
@@ -45,7 +45,7 @@ const CountrySet = (CountryArray) => {
 function DetailsShow(name,code){
     const CountryDiv = document.getElementById(`${code}`);    
     const CountryHoverDiv = document.getElementById(`${code}-Details`); 
-    CountryDiv.style.display="none";
+    CountryDiv.style.display="block";
     CountryHoverDiv.style.display="block";
 }
 function DetailsOff(name,code){
